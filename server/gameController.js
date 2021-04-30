dd468000ad9496f05143b915704468d27c4431ea
alex <alexanderstapp@gmail.com>
2021-04-30 11:26:41 -0600

    backend/server build complete

---
+++ server/gameController.js
@@ -0,0 +1,55 @@
+const { getMovies } = require("../../wlr1-nodb-full-review/server/controllers/movieController")
+
+let games = [
+    {
+        id: 1,
+        completed: false,
+        title: 'gametest',
+        platform: 'xbone',
+        hours: 40
+    },
+    {
+        id: 2,
+        completed: false,
+        title: 'mario game',
+        platform: 'wiiu',
+        hours: 10
+    }
+]
+let id = 3
+
+module.exports = {
+    getGames: (_req, res) => {
+      res.status(200).send(games)  
+    },
+
+    addGame: (req, res) => {
+        const {title, platform, hours} = req.body
+        const game = {
+            id: id++,
+            completed: false,
+            title,
+            platform,
+            hours
+        }
+        games.push(game)
+        res.status(200).send(games)
+    },
+    
+    deleteGame: (req, res) => {
+        const {id} = req.params
+        const index = games.findIndex(e => e.id === +id)
+        games.splice(index, 1)
+        res.status(200).send(games)
+    },
+
+    editGame: (req, res) => {
+        const {id} = req.params
+        const {title, platform, hours} = req.body
+        const index = games.findIndex(e => e.id === +id)
+        games[index].title = title
+        games[index].platform = platform
+        games[index].hours = hours
+        res.status(200).send(games)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const express = require('express')
 const app = express()
 const PORT = 3030
 
-const tc = require('./titleController')
+const tc = require('./gameController')
 
 app.use(express.json())
 
-// app.get
-// app.post
+app.get('/api/games', tc.getGames)
+app.post('/api/games', tc.addGame)
+app.delete('/api/games/:id', tc.deleteGame)
+app.put('/api/games/:id', tc.editGame)
 
 app.listen(PORT, () => console.log(`server is rinning on ${PORT} :3`))
